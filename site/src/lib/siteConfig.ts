535d03826bc66f3c9150792e9e6ca7b3f8d7101d
armentrout1 <aaron@aprkc.com>
2026-01-19 21:25:15 -0600

    Add site config, integrate header/footer/mobile CTA, build homepage content

---
+++ site/src/lib/siteConfig.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  name: "ACE Properties KC",
+  domain: "acepropertieskc.com",
+  baseUrl: "https://acepropertieskc.com",
+  phoneDisplay: "555-555-5555", // TODO: replace
+  phoneTel: "+15555555555", // TODO: replace
+  smsTel: "+15555555555", // TODO: replace
+  email: "info@acepropertieskc.com", // TODO: replace
+  serviceArea: "Kansas City metro (including ZIP 64131)",
+  defaultTitle: "ACE Properties KC | Cash & Creative Home Buying Solutions",
+  defaultDescription:
+    "Local Kansas City metro home buyer. Sell as-is for cash or explore creative options. No repairs, no pressure. Start with a quick conversation.",
+};
